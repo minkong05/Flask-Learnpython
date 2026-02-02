a4845cbf0ce96f4541c12563922acc989bf5997e
minkong05 <186683024+minkong05@users.noreply.github.com>
2026-02-02 15:17:33 +0000

    Update pytest

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,9 +2,22 @@ def test_login_without_data(client):
     res = client.post("/login", json={})
     assert res.status_code == 400
 
+    data = res.get_json()
+    assert data["message"] == "No data received"
+
 def test_login_invalid_credentials(client):
     res = client.post("/login", json={
         "email": "fake@example.com",
         "password": "wrong"
     })
     assert res.status_code == 401
+
+
+def test_login_missing_password(client):
+    res = client.post("/login", json={
+        "email": "test@example.com"
+    })
+    assert res.status_code == 400
+
+    data = res.get_json()
+    assert data["message"] == "Email or password missing"
