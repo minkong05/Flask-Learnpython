dcb3f2de18329bc04d4cf91713d7028ec4a530c5
minkong05 <186683024+minkong05@users.noreply.github.com>
2026-02-05 20:18:52 +0000

    locked-in TESTING at import time

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ from app import app as flask_app
 @pytest.fixture
 def app():
     flask_app.config.update({
-        "TESTING": True,
         "WTF_CSRF_ENABLED": False,  # disable CSRF in tests
         "SECRET_KEY": "test-secret"
     })
